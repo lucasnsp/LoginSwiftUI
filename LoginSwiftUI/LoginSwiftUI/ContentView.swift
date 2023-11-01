f40c214baae0417055f7e9087368dc1bc2288fc0
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-01 13:30:05 -0300

    Add files via upload

---
+++ LoginSwiftUI/LoginSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 01/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginSwiftUI/LoginSwiftUI/LoginSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIApp.swift
+//  LoginSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
