1a8bba8389ef07ddcfaf93299b7dd43ae9bfe48d
Lucas Neves <lucas.neves12@icloud.com>
2023-11-02 10:45:32 -0300

    Finished creating Login Screen

--- LoginSwiftUI/LoginSwiftUI/ContentView.swift
+++ LoginSwiftUI/LoginSwiftUI/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     @State var email: String = ""
+    @State var password: String = ""
     
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea(edges: .all)
-            VStack {
+            VStack(spacing: 10.0) {
                 Text("Login")
                     .foregroundStyle(.white)
                     .font(.system(size: 40, weight: .bold))
@@ -29,10 +30,41 @@ struct ContentView: View {
                     .cornerRadius(22)
                     .foregroundStyle(.black)
                     .keyboardType(.emailAddress)
-                    .padding(.horizontal, 20)
+                
+                SecureField("Digite sua senha:", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .cornerRadius(22)
+
+                    .foregroundStyle(.black)
+                    .keyboardType(.default)
+                
+                Button {
+                    print("Clique no botão!!")
+                } label: {
+                    Text("Login")
+                        .frame(height: 40)
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20, weight: .bold))
+                        .background(Color(red: 228/255, green: 50/255, blue: 128/255))
+                        .cornerRadius(22)
+                }
+                
+                HStack {
+                    Text("Não tem conta?")
+                        .foregroundStyle(.white)
+                    Button {
+                        print("Clique no botão")
+                    } label: {
+                        Text("Cadastre-se")
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundStyle(Color(red: 228/255, green: 58/255, blue: 128/255))
+                    }
+                }
                 
                 Spacer()
             }
+            .padding(.horizontal, 20)
         }
     }
 }
